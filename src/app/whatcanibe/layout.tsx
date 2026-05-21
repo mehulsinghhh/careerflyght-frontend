91bef745b259e1adc3451b911b1aaf09458c94e3
Mehul <mehulsingh301@gmail.com>
2026-05-22 05:20:08 +0530

    Revert "Feature/whatcanibe UI polish 13135479770845113835"

--- src/app/whatcanibe/layout.tsx
+++ src/app/whatcanibe/layout.tsx
@@ -7,13 +7,7 @@ export default function WhatCanIBeLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen flex-col bg-black text-white selection:bg-violet-500/30">
-      {/* Noise Overlay */}
-      <div
-        className="fixed inset-0 pointer-events-none z-[100] opacity-[0.03] mix-blend-overlay"
-        style={{ backgroundImage: "url('https://grainy-gradients.vercel.app/noise.svg')" }}
-      />
-
+    <div className="flex min-h-screen flex-col bg-black text-white">
       <WhatCanIBeNavbar />
       <main className="flex-1">{children}</main>
       <WhatCanIBeFooter />
